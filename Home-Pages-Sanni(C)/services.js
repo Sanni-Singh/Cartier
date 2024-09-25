091ce0f1b7fd419edd2863ae2c1568b58ee2a8a0
Sanni-Singh <aryansingh7371@gmail.com>
2024-09-25 15:06:10 +0530

    adding the hameBurgur of homePage & adding news Section

---
+++ Home-Pages-Sanni(C)/services.js
@@ -0,0 +1,19 @@
+function onClickMenu(){
+    console.log("hmelllo poolice");
+    document.getElementById("menu").classList.toggle("icon");
+    console.log("hmelllo poolice");
+    document.getElementById("nav").classList.toggle("change");
+
+}
+// const element = document.getElementById("stikyNavbar")
+// function stikyHeader(){
+//     const position =element.getBoundingClientRect();
+//     if(position.top <= 20){
+//         element.classList.add("active");
+        
+//     }
+//     else{
+//         element.classList.remove("active");
+//     }
+// }
+// window.addEventListener("scroll" ,stikyHeader);
---
+++ navbar&footer/navbar.js
@@ -0,0 +1,7 @@
+function onClickMenu(){
+    console.log("hmelllo poolice");
+    document.getElementById("menu").classList.toggle("icon");
+    console.log("hmelllo poolice");
+    document.getElementById("navv").classList.toggle("change");
+
+}
