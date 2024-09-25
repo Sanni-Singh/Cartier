ad955bd2fa8e731b5da26eb569947a338a672c0d
Sanni-Singh <aryansingh7371@gmail.com>
2024-09-26 01:09:26 +0530

    coonection the link of some files

--- navbar&footer/navbar.js
+++ navbar&footer/navbar.js
@@ -1,16 +1,5 @@
 function onClickMenu(){
-    // console.log("hmelllo poolice");
     document.getElementById("menu").classList.toggle("icon");
-    // console.log("hmelllo poolice");
     document.getElementById("navv").classList.toggle("change");
 
 }
-
-// const menu = document.getElementById('menu');
-//     const navv = document.querySelector('.navv');
-//     const bars = document.querySelectorAll('.bar');
-
-//     menu.addEventListener('click', () => {
-//         navv.classList.toggle('show');
-//         bars.forEach(bar => bar.classList.toggle('icon'));
-//     });
